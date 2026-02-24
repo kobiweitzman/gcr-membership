b15b38ef9eee81e0ca34322128d19adecdcfa57d
kobiweitzman <kobiweitzman@gmail.com>
2026-02-24 11:46:48 -0500

    Initial Commit

---
+++ GCR Membership/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GCR Membership
+//
+//  Created by Kobi Weitzman on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GCR Membership/GCR_MembershipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCR_MembershipApp.swift
+//  GCR Membership
+//
+//  Created by Kobi Weitzman on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct GCR_MembershipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
